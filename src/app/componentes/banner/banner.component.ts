94b8df5d3795333d5cf6b72bb46d4ae09a0343f5
Elflacomou <elflacomurel@hotmail.com>
2023-04-30 10:49:55 -0300

    Creacion de servicio, commit en rama servicio

--- src/app/componentes/banner/banner.component.ts
+++ src/app/componentes/banner/banner.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent {
+export class BannerComponent  implements OnInit {
+  constructor (private datosPortfolio:PortfolioService) {}
 
+  ngOnInit():void {
+    this.datosPortfolio.obtenerDatos();
+  }
 }
